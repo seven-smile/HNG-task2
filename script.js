e00f04458c36bac8f77376fa002eaa7c43ad9c05
Miles <juniornyamile@gmail.com>
2024-07-02 14:11:48 +0000

    correctins

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 document.addEventListener("DOMContentLoaded", function() {
     function updateTimeAndDay() {
         const now = new Date();
-        const utcTime =  now.toUTCString().split(" ")[4];
+        const utcTime = "Current Time: " + " "+  now.toUTCString().split(" ")[4];
         const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-        const currentDay = days[now.getUTCDay()];
+        const currentDay = "Current Day of the Week: " + " "+ days[now.getUTCDay()];
 
         document.querySelector('[data-testid="currentTimeUTC"]').textContent = utcTime;
         document.querySelector('[data-testid="currentDay"]').textContent = currentDay;
